4715a200936f8bad500ac1739aea9dab287f6741
Kacper Marczewski <kacper.marczewski@gmail.com>
2023-10-06 22:47:06 +0200

    pimes calculation

--- test1.rs
+++ test1.rs
@@ -1,6 +1,25 @@
 fn main() {
-    // Statements here are executed when the compiled binary is called
+    const FROM: u128 = 200;
+    const TO: u128 = 220;
+    let limit: u128 = (TO as f64).sqrt() as u128;
+    let mut a: [u8; (TO - FROM) as usize] = [0; (TO - FROM) as usize]; 
 
-    // Print text to the console
-    println!("Hello World!");
+    for n in 0..(TO - FROM) as usize {
+        a[n] = 0;
+    }
+
+    for n in FROM..TO {
+        for i in 2..limit {
+            if n % i == 0 {
+                a[(n - FROM) as usize] = 1;
+                break;
+            }
+        }
+    }
+
+    for n in FROM..TO {
+        if a[(n - FROM) as usize] == 0 {
+            println!("{}",n);
+        }
+    }
 }
