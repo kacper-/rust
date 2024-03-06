7d8371295e3228ef1b1a1288aeb3c57b4e8e02b3
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-03-06 23:54:10 +0100

    sequence

--- test2.rs
+++ test2.rs
@@ -1,3 +1,19 @@
 fn main() {
-    
+    let member = 2_usize.pow(25) + 1;
+
+    let mut n1: i32 = 1;
+    let mut n2: i32 = 2;
+    let mut n3: i32 = 3;
+
+    let mut n = 3;
+
+    while n < member
+    {
+        let new: i32 = n1 - 2 * &n2 + 3 * &n3;
+        n1 = n2;
+        n2 = n3;
+        n3 = new % 1000000;
+        n += 1;
+    }
+    println!("{0}", n3);
 }
