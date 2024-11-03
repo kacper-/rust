219c67b6c7d7d56e51c81f32641c6a7d6c379b09
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-11-03 22:43:14 +0100

    ga

--- test2.rs
+++ test2.rs
@@ -1,5 +1,27 @@
-fn main() {
- let x: [i8; 10] = {1,2,3,4,5,6,7,8,9,0};
- let y: [i8; 10] = {1,2,3,4,5,6,7,8,9,0};
- for
+fn print_number(a: &[i8]) {
+    for i in 0..a.len() {
+        print!("{}", a[i]);
+    }
 }
+
+fn main() {
+ let x: [i8; 10] = [1,2,3,4,5,6,7,8,9,0];
+ let y: [i8; 10] = [1,2,3,4,5,6,7,8,9,0];
+ let r: [i8; 20] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+ for i in 0..x.len() {
+    for j in 0..y.len() {
+
+    }
+ }
+ print!("x = ");
+ print_number(&x);
+ println!("");
+ print!("y = ");
+ print_number(&y);
+ println!("");
+ print!("r = ");
+ print_number(&r);
+ println!("");
+}
+
