d159c01fad8a5841ba79a3f2dc881efd6451790c
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-11-03 17:20:05 +0100

    multiplication

--- test2.rs
+++ test2.rs
@@ -1,19 +1,5 @@
 fn main() {
-    let member = 2_usize.pow(25) + 1;
-
-    let mut n1: i32 = 1;
-    let mut n2: i32 = 2;
-    let mut n3: i32 = 3;
-
-    let mut n = 3;
-
-    while n < member
-    {
-        let new: i32 = n1 - 2 * &n2 + 3 * &n3;
-        n1 = n2;
-        n2 = n3;
-        n3 = new % 1000000;
-        n += 1;
-    }
-    println!("{0}", n3);
+ let x: [i8; 10] = {1,2,3,4,5,6,7,8,9,0};
+ let y: [i8; 10] = {1,2,3,4,5,6,7,8,9,0};
+ for
 }
