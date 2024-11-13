56c1d4c8e8a4d5570271d4371652b4371da5c65f
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-11-13 22:20:26 +0100

    implementaion start

--- test2.rs
+++ test2.rs
@@ -5,13 +5,24 @@ fn print_number(a: &[i8]) {
 }
 
 fn main() {
- let x: [i8; 10] = [1,2,3,4,5,6,7,8,9,0];
- let y: [i8; 10] = [1,2,3,4,5,6,7,8,9,0];
- let r: [i8; 20] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+ let mut x: [i8; 10] = [1,2,3,4,5,6,7,8,9,0];
+ let mut y: [i8; 10] = [1,2,3,4,5,6,7,8,9,0];
+ let mut r: [i8; 20] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+ let mut temp: i8 = 0;
 
  for i in 0..x.len() {
     for j in 0..y.len() {
-
+        temp = x[i]*y[j];
+        r[i+j] += temp % 10;
+        if r[i+j] > 9 {
+            r[i+j+1] += r[i+j] / 10; 
+            r[i+j] += r[i+j] % 10;
+        }
+        r[i+j+1] += temp/10;
+        if r[i+j+1] > 9 {
+            r[i+j+2] += r[i+j+1] / 10; 
+            r[i+j+1] += r[i+j+1] % 10; 
+        }
     }
  }
  print!("x = ");
@@ -19,7 +30,7 @@ fn main() {
  println!("");
  print!("y = ");
  print_number(&y);
- println!("");
+ println!("");   
  print!("r = ");
  print_number(&r);
  println!("");
