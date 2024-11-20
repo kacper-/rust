da7a5503ea7dc3ee965c496d12814323004e49be
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-11-20 01:17:34 +0100

    fixed

--- test2.rs
+++ test2.rs
@@ -2,37 +2,33 @@ fn print_number(a: &[i8]) {
     for i in 0..a.len() {
         print!("{}", a[i]);
     }
+    println!("");
 }
 
 fn main() {
- let mut x: [i8; 10] = [1,2,3,4,5,6,7,8,9,0];
- let mut y: [i8; 10] = [1,2,3,4,5,6,7,8,9,0];
- let mut r: [i8; 20] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
- let mut temp: i8 = 0;
+ let x: [i8; 3] = [1,2,3];
+ let y: [i8; 3] = [1,2,3];
+ let mut r: [i8; 6] = [0,0,0,0,0,0];
+ let mut temp: i8;
+ let mut kpos: usize;
 
  for i in 0..x.len() {
     for j in 0..y.len() {
-        temp = x[i]*y[j];
-        r[i+j] += temp % 10;
-        if r[i+j] > 9 {
-            r[i+j+1] += r[i+j] / 10; 
-            r[i+j] += r[i+j] % 10;
-        }
-        r[i+j+1] += temp/10;
-        if r[i+j+1] > 9 {
-            r[i+j+2] += r[i+j+1] / 10; 
-            r[i+j+1] += r[i+j+1] % 10; 
-        }
+        temp = x[x.len()-i-1]*y[y.len()-j-1];
+        r[r.len()-i-j-1] += temp;
+    }
+    for k in 0..r.len()-1 {
+        kpos = r.len()-k-1;
+        r[kpos-1] += r[kpos] / 10;
+        r[kpos] = r[kpos] % 10; 
     }
  }
+ println!("");  
  print!("x = ");
  print_number(&x);
- println!("");
  print!("y = ");
  print_number(&y);
- println!("");   
  print!("r = ");
  print_number(&r);
- println!("");
 }
 
