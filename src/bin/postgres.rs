cef93b7bc93aea928d07ad26786d1e0d373288e3
Kacper Marczewski <kacper.marczewski@gmail.com>
2026-02-27 01:12:47 +0100

    postgres test

--- src/bin/postgres.rs
+++ src/bin/postgres.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+extern crate postgres;
+
+use postgres::{Client, NoTls, Error};
+use std::collections::HashMap;
+
+struct Author {
+    _id: i32,
+    name: String,
+    country: String
+}
+
+fn main() -> Result<(), Error> {
+    let mut client = Client::connect("postgresql://postgres:postgres@localhost/library",
+                                     NoTls)?;
+
+    let mut authors = HashMap::new();
+    authors.insert("Chinua Achebe", "Nigeria");
+    authors.insert("Rabindranath Tagore", "India");
+    authors.insert("Anita Nair", "India");
+
+    for (key, value) in &authors {
+        let author = Author {
+            _id: 0,
+            name: key.to_string(),
+            country: value.to_string()
+        };
+
+        client.execute(
+            "INSERT INTO author (name, country) VALUES ($1, $2)",
+            &[&author.name, &author.country],
+        )?;
+    }
+
+    for row in client.query("SELECT id, name, country FROM author", &[])? {
+        let author = Author {
+            _id: row.get(0),
+            name: row.get(1),
+            country: row.get(2),
+        };
+        println!("Author {} is from {}", author.name, author.country);
+    }
+
+    Ok(())
+
 }
